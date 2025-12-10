0d10156116ddfc0f49d994bac79f141472479a44
Misha1177 <mmmirzaev@edu.hse.ru>
2025-12-10 19:50:09 +0300

    siu

--- bot/app/core.py
+++ bot/app/core.py
@@ -39,7 +39,6 @@ def increment_question_count(user_id: int):
     meta = ensure_user_meta(user_id)
     meta["questions_used"] += 1
 
-
 def can_ask_question(user_id: int) -> (bool, str):
     """
     тут проверка, может ли пользователь задать очередной вопрос.
--- bot/app/keyboards.py
+++ bot/app/keyboards.py
@@ -55,7 +55,6 @@ def models_menu_kb(provider_code: str) -> InlineKeyboardMarkup:
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
-
 def settings_menu_kb() -> InlineKeyboardMarkup:
     """Меню настроек (/settings)"""
     keyboard = [
